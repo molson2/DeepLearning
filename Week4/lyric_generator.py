e8d935416092e602fac144d3d5695f33e2fd5c92
molson2 <molson5574@gmail.com>
2017-10-26 23:23:29 -0400

    added code to generate lyrics from final model

--- Week4/lyric_generator.py
+++ Week4/lyric_generator.py
@@ -5,14 +5,14 @@ RM_MAX = 1000000
 #                      Read in / process lyrics
 # ------------------------------------------------------------------------------
 
-with open('bible.txt', 'r') as f:
+with open('lyrics.csv', 'r') as f:
     lyrics = f.read()
 
 # remove titles, make everything lowercase
 lyrics = re.sub(r'##[A-Z]+##', '', lyrics, RM_MAX)
-lyrics = re.sub(r'\x80|\x93|\x94|\x98|\x99|\x9c|\x9d|\xa6|\xad|\xaf|\xc2|\xc3|\xe2|\xa9|\xa8|\xa1|\xa0|\xa7|\xef|\xbb|\xbf|-',
-                '', lyrics, RM_MAX)
-lyrics = re.sub(r'\$|#|&|\*|\{|\}|/|;|\)|\(|', '', lyrics, RM_MAX)
+lyrics = re.sub(
+    r'\x80|\x93|\x94|\x98|\x99|\x9c|\x9d|\xa6|\xad|\xaf|\xc2|\xc3|\xe2|\xa9|\xa8|\xa1|\xa0|\xa7', '', lyrics, RM_MAX)
+lyrics = re.sub(r'\$|#|&|\*|[0-9]|\{|\}|/|;|\)|\(|:', '', lyrics, RM_MAX)
 lyrics = lyrics.lower()
 
 vocab = list(set(lyrics))
@@ -25,7 +25,15 @@ zip(vocab, counts)
 
 
 import lstm_models
+import tensorflow as tf
 
+# fit the model
 lyric_gen = lstm_models.LSTMLyrics(
-    lyrics, n_neurons=512, n_layers=2, n_steps=50, save_path='bible.ckpt')
-lyric_gen.fit(n_epochs=100, batch_size=128, eta=0.001, nchar=200)
+    lyrics, n_neurons=512, n_layers=2, n_steps=50, save_path='lyrics.ckpt')
+lyric_gen.fit(n_epochs=100, batch_size=128, eta=0.01, nchar=200)
+
+# write a big chunk of lyrics from final model
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    saver.restore(sess, lyric_gen.save_path)
+    sample_lyrics = lyric_gen.sample(1000, sess)
